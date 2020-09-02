5e5ce2d9a8256ff522b044f223c9a0e11f0f6f5d
Zach <54759064+ZACHSTRIVES@users.noreply.github.com>
2020-09-02 20:10:57 +1200

    Dashbord/登录状态

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config.from_object(config)
 def login_page():
     if request.method == 'GET':
         return render_template('index.html')
-    if request.method == 'POST' and len(request.form)==5:
+    if request.method == 'POST' and len(request.form) == 5:
         name = request.form.get('name')
         email = request.form.get('email')
         password_1 = request.form.get('password_1')
@@ -55,7 +55,7 @@ def login_page():
 
         except Exception as e:
             raise e
-    elif request.method == 'POST' and len(request.form)==2:
+    elif request.method == 'POST' and len(request.form) == 2:
         email = request.form.get('email')
         password = request.form.get('password')
         if not all([email, password]):
@@ -72,7 +72,7 @@ def login_page():
             if check_password_hash(result[0], password):
                 session['email'] = email
                 session.permanent = True
-                return render_template('backend.html')
+                return dashbord()
             else:
                 flash("Incorrect password!")
                 return render_template('index.html')
@@ -80,9 +80,43 @@ def login_page():
             raise e
 
 
-# @app.route('/backend')
-# def backend():
-#     return render_template('backend.html')
+# Login status maintained
+@app.context_processor
+def login_status():
+    # from session get email
+    email = session.get('email')
+    # If there is email information, it proves that you are logged in, and we
+    # get the nickname and user type of the logon from the database to return to the global
+    if email:
+        try:
+            cur = db.cursor()
+            sql = "select name,department from user where email = '%s'" % email
+            db.ping(reconnect=True)
+            cur.execute(sql)
+            result = cur.fetchone()
+            if result:
+                return {'email': email, 'name': result[0], 'department': result[1]}
+        except Exception as e:
+            raise e
+    # If email information does not exist, no login, return empty
+    return {}
+
+
+@app.route('/backend',methods=['GET'])
+def dashbord():
+    login_=login_status()
+    print(login_)
+    if len(login_)==0:
+        return render_template('index.html')
+    else:
+        return render_template('backend.html',issue_information=login_['name'])
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
